627cf0395fc293fd2d6fdcf7c4a8ebd8794c66af
SauloMota <sauloarlindopereirapereira@gmail.com>
2026-09-24 20:29:59 -0300

    Criado o chessMatch

---
+++ src/UI.java
@@ -0,0 +1,25 @@
+import chess.ChessPiece;
+
+public class UI {
+
+    pubic static void printBoard(ChessPiece[][] pieces){
+        for(int i = 0 ; i < pieces.length; i++){
+            System.out.print((8 - 1) + " ");
+            for(int j = 0 ; j < pieces.length; j++){
+                System.out.print(pieces[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("a b c d e f g h");
+        
+    }
+
+    private static void printPiece(ChessPiece piece){
+        if(piece == null){
+            System.out.print("-");
+        }else{
+            System.out.print(piece);
+        }
+        System.err.print(" ");
+    }
+}
