9523ae49be9b1bc41a0dc807fd7bc0098c344ffa
SauloMota <sauloarlindopereirapereira@gmail.com>
2026-09-23 22:18:21 -0300

    Adicionado as classs Board e Piece

--- src/App.java
+++ src/App.java
@@ -1,9 +1,9 @@
 
-import boardgame.Position;
+import boardgame.Board;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Position pos = new Position(3, 5);
-        System.out.println(pos);
+        Board board = new Board(8, 8);
+        
     }
 }
