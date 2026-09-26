ab1ca28384fc7b956faf610382ba087fc94f8143
SauloMota <sauloarlindopereirapereira@gmail.com>
2026-09-25 22:08:15 -0300

    Criado a Proteção e exceçoes de Board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int columns, int rows) {
+        if (rows < 1 || columns < 1){
+            throw new BoardException("Deve ter pelo menos 1 coluna e 1 linha");
+        }
         this.columns = columns;
         this.rows = rows;
         pieces = new Piece[rows][rows];
@@ -14,24 +17,43 @@ public class Board {
     public int getRows() {
         return rows;
     }
-
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
-
     public Piece piece(int row,int column){
+        if(!positionExists(row,column)){
+            throw new BoardException("Posição fora do tabuleiro");
+        }
         return pieces[row][column];
     }
+
     public Piece piece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Posição fora do tabuleiro");
+        }
         return pieces[position.getRow()][position.getColumn()];
+    }
+
+    public void placePiece(Piece piece , Position position){
+        if (thereIsAPiece(position)){
+            throw new BoardException("Ja tem uma peça neste local");
+        }
+        pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
+    private boolean positionExists(int row , int column){
+        return row >= 0 && row < rows && column >= 0  && column < columns;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow() , position.getColumn());
+    }
+    public boolean thereIsAPiece(Position position){
+        if(!positionExists(position)){
+            throw new BoardException("Posição fora do tabuleiro");
+        }
+        return piece(position) != null;
     }
 
 }
